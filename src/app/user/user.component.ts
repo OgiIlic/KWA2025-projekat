7ffe1cbe235ff2eaaaef85289e305b7e685c20c3
OgiIlic <ognjen.ilic.22@singimail.rs>
2025-03-19 10:58:23 +0100

    Added change info panel

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,16 +2,27 @@ import { Component, input } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { Router, RouterLink } from '@angular/router';
 import { OrderModel } from '../../models/order.model';
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { UserModel } from '../../models/user.model';
 import { MatTableModule } from '@angular/material/table';
 import { UtilsService } from '../../services/utils.service';
+import {MatAccordion, MatExpansionModule} from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { GenreModel } from '../../models/genre.model';
+import { MovieService } from '../../services/movie.service';
 
 @Component({
   selector: 'app-user',
-  imports: [NgIf, MatButtonModule, MatCardModule, MatTableModule, RouterLink],
+  imports: 
+  [NgIf, NgFor, MatButtonModule, MatCardModule, MatTableModule, RouterLink,
+    MatExpansionModule, MatAccordion, MatFormFieldModule, MatInputModule,
+    FormsModule, MatSelectModule
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
@@ -19,6 +30,10 @@ export class UserComponent {
 
   public displayedColumns: string[] = ['title', 'count', 'pricePerCard', 'price', 'status', 'actions']
   public user: UserModel | null = null
+  public userCopy: UserModel | null = null
+  public newPasswordValue = ''
+  public repeatPasswordValue= ''
+  public genreList: GenreModel[] | null = null
 
   constructor(private router: Router, public utils: UtilsService) {
     if(!UserService.getActiveUser()) {
@@ -27,16 +42,33 @@ export class UserComponent {
       return
     }
 
-    this.user = UserService.getActiveUser(  )
+    this.user = UserService.getActiveUser()
+    this.userCopy = UserService.getActiveUser()
+    MovieService.getGenres()
+    .then(rsp => this.genreList = rsp.data)
   }
 
+  public doUpdateInfo() {
+    if(this.userCopy == null){
+      alert('Korisnik nije definisan')
+      return
+    }
+    UserService.updateUser(this.userCopy)
+    this.user = UserService.getActiveUser()
+    alert('Korisnik je uspešno ažuriran')
+  }
   public doChangePassword() {
-    const newPassword = prompt('Enter your new password')
-    if(newPassword == '' || newPassword == null) {
+    if(this.newPasswordValue == '' || this.newPasswordValue == null) {
       alert('Password cant be empty')
       return
     }
-    alert(UserService.changePassword(newPassword)?'Password chaned successfully': 'Failed to change password')
+
+    if(this.newPasswordValue !== this.repeatPasswordValue) {
+      alert('Lozinke se ne poklapaju')
+      return
+    }
+
+    alert(UserService.changePassword(this.newPasswordValue)?'Lozinka promenjena uspešno': 'Failed to change password')
     }
 
   public doPay(order: OrderModel) {
